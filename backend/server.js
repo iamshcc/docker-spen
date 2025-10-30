8d962165e7c21159fcd6fc94300272342d99d41e
ShCc <iamshcc@gmail.com>
2025-10-30 11:41:02 +0000

    feat: add nginx service

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,12 @@ const app = express();
 const prisma = new PrismaClient();
 
 app.use(express.json());
-app.use(cors());
+
+app.use(cors({
+  origin: "http://localhost:3000",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"]
+}));
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello Dear!" });
